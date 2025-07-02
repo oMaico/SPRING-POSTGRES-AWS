57c2377ebc3d156d8ba4e3c9bed8aca8f2b4ec5b
oMaico <maychonsouza678@gmail.com>
2025-07-02 15:25:58 -0300

    FEAT: INIT

---
+++ src/test/java/com/uskhalifa/eventoapi/EventoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uskhalifa.eventoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
